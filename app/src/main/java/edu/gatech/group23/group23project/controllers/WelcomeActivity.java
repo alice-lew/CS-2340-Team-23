db5c60ccc104f210dc4ac95e4574295db06851e5
PantslessGrandpa <noahablume@gmail.com>
2017-02-12 13:18:30 -0500

    first commit - with half-working login

---
+++ app/src/main/java/edu/gatech/group23/group23project/controllers/WelcomeActivity.java
@@ -0,0 +1,36 @@
+package edu.gatech.group23.group23project.controllers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import edu.gatech.group23.group23project.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+        TextView instructionsText = (TextView) findViewById(R.id.welcomeInstructionsText);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        Button registerButton = (Button) findViewById(R.id.registerButton);
+        welcomeText.setText("Welcome!");
+        instructionsText.setText("Please sign in or register an account.");
+        loginButton.setText("SIGN IN");
+        registerButton.setText("REGISTER");
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                Intent intent = new Intent(context, LoginActivity.class);
+                context.startActivity(intent);
+            }
+        });
+    }
+}

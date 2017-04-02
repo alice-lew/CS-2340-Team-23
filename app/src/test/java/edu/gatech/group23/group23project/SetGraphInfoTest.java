921858d2a0c5670a5d82e790cac1c6493dd98e6d
PantslessGrandpa <noahablume@gmail.com>
2017-04-02 18:59:43 -0400

    made junit test for setGraphInfo

---
+++ app/src/test/java/edu/gatech/group23/group23project/SetGraphInfoTest.java
@@ -0,0 +1,18 @@
+package edu.gatech.group23.group23project;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * A unit test for the model's getInstance method
+ * Created by Mbrune on 4/2/2017.
+ */
+public class ModelGetInstanceTest {
+    @Before
+
+
+    @Test
+    public void modelIsNullTest() {
+        
+    }
+}
